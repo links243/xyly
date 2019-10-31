ee1870c505360df424fbb566244d824d042cc373
梁超 <liangchao13@foxmail.com>
2019-10-31 19:36:09 +0800

    完成分页，完成筛选的静态数据展示，暂时筛选事件不可用

--- components/air/FlightsFilter.vue
+++ components/air/FlightsFilter.vue
@@ -1,44 +1,132 @@
 <template>
   <div class="flights_filter">
     <div class="filter_main">
-      <div class="main_path">单程: 广州-上海/ 2019-10-28 </div>
-      <div class="main_selects">
-        <div class="select_item"> <el-select placeholder="请选择" size="mini" > <el-option v-for="item in  [{ value: '选项1', label: '黄金糕' }]" :key="item.value" :label="item.label" :value="item.value" > </el-option> </el-select> </div>
-        <div class="select_item"> <el-select placeholder="请选择" size="mini" > <el-option v-for="item in  [{ value: '选项1', label: '黄金糕' }]" :key="item.value" :label="item.label" :value="item.value" > </el-option> </el-select> </div>
-        <div class="select_item"> <el-select placeholder="请选择" size="mini" > <el-option v-for="item in  [{ value: '选项1', label: '黄金糕' }]" :key="item.value" :label="item.label" :value="item.value" > </el-option> </el-select> </div>
-        <div class="select_item"> <el-select placeholder="请选择" size="mini" > <el-option v-for="item in  [{ value: '选项1', label: '黄金糕' }]" :key="item.value" :label="item.label" :value="item.value" > </el-option> </el-select> </div>
+      <div class="main_path" @click="aaa">单程: 广州-上海/ 2019-10-28</div>
+      <div class="main_selects" v-if="flightsData.flights.length">
+        <div class="select_item">
+          <el-select placeholder="起飞机场" size="mini" v-model="airport">
+            <el-option
+              v-for="item in filterData.airport"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="select_item">
+          <el-select placeholder="起飞时间" size="mini" v-model="flightTimes">
+            <el-option
+              v-for="item in filterData.flightTimes"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="select_item">
+          <el-select placeholder="航空公司" size="mini" v-model="company">
+            <el-option
+              v-for="item in  filterData.company"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+        <div class="select_item">
+          <el-select placeholder="机型" size="mini" v-model="sizes">
+            <el-option
+              v-for="item in  filterData.planeSize"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
       </div>
     </div>
     <div class="filters_btns">
-      筛选: <el-button type="primary" round size="mini"   >撤销</el-button>
+      筛选:
+      <el-button type="primary" round size="mini">撤销</el-button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["flightsData"],
+  data() {
+    return {
+      // 起飞机场默认值
+      airport: "",
+      // 起飞时间
+      flightTimes: "",
+      // 航空公司
+      company: "",
+      // 机型
+      sizes: ""
+    };
+  },
+  computed: {
+    filterData() {
+      let airport = [];
+      let flightTimes = [];
+      let company = [];
+      let planeSize = [];
+      // 起飞机场
+      this.flightsData.options.airport.forEach(val => {
+        val && airport.push({ value: val, label: val });
+      });
+
+      // 起飞时间
+      flightTimes = this.flightsData.options.flightTimes.map(val => ({
+        label: `${val.from}:00 - ${val.to}:00`,
+        value: `${val.from}|${val.to}`
+      }));
+
+      // 航空公司
+      company = this.flightsData.options.company.map(val => ({
+        label: val,
+        value: val
+      }));
+
+      // 机型
+      planeSize = [
+        {value: 'S' ,label: '小型机型'},
+        {value: 'M' ,label: '中等机型'},
+        {value: 'L' ,label: '大型机型'}
+      ]
+      return { airport, flightTimes, company, planeSize };
+    }
+  },
+  methods: {
+    aaa() {
+      console.log(this.filterData);
+      console.log(this.flightsData);
+    }
+  }
+};
 </script>
 <style lang="less" scoped>
-.flights_filter{
-
-  .filter_main{
+.flights_filter {
+  .filter_main {
     height: 56px;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    .main_path{
+    .main_path {
       font-size: 13px;
     }
-    .main_selects{
+    .main_selects {
       display: flex;
-      
-      .select_item{
+
+      .select_item {
         width: 120px;
         margin-left: 5px;
       }
     }
   }
-  .filters_btns{
+  .filters_btns {
     padding: 10px 0;
   }
 }
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -3,28 +3,39 @@
     <!-- 正文 开始 -->
     <div class="flights_main">
       <!-- 1 筛选模块 开始 -->
-      <FlightsFilter v-if='false'/>
+      <FlightsFilter
+       :flightsData="flightsData"
+       />
       <!-- 1 筛选模块 结束 -->
 
       <!-- 2 表单的头部 开始 -->
-      <FlightsHead/>
+      <FlightsHead />
       <!-- 2 表单的头部 结束 -->
 
       <!-- 3 机票列表 开始 -->
       <div class="air_list">
-        <FlightsItem 
-          v-for="item in flightsData.flights" :key="item.id"
-          :data='item'
-        />
+        <FlightsItem v-for="item in currentFlights" :key="item.id" :data="item" />
       </div>
       <!-- 3 机票列表 结束 -->
+      <!-- 分页功能开始 -->
+      <div>
+        <el-pagination
+          :current-page="page.currentPage"
+          :page-sizes="page.pageSizes"
+          :page-size="page.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="page.total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        ></el-pagination>
+      </div>
+      <!-- 分页功能结束 -->
     </div>
     <!-- 正文 结束 -->
 
     <!-- 侧边栏 开始 -->
     <div class="flights_aside">2</div>
     <!-- 侧边栏 结束 -->
-
   </div>
 </template>
 
@@ -35,20 +46,84 @@ import FlightsItem from "@/components/air/FlightsItem";
 export default {
   data() {
     return {
-      flightsData:{}
-    }
+      // 从服务器获取的机票原始数据
+      flightsData: {
+        info: {},
+        flights: [],
+        options: {}
+      },
+
+      // 分页对象
+      page: {
+        // 当前页码
+        currentPage: 1,
+        // 页容量数组
+        pageSizes: [1, 2, 5, 10, 20, 100],
+        // 页容量
+        pageSize: 10,
+        // 总条数
+        total: 1
+      },
+
+      // 被分页后的 机票列表
+      currentFlights: []
+    };
   },
   components: {
-    FlightsFilter,FlightsHead,FlightsItem
+    FlightsFilter,
+    FlightsHead,
+    FlightsItem
+  },
+  methods: {
+    // 获取机票数据
+    getList(isFrist) {
+      if (isFrist) {
+        let form = this.$route.query;
+        this.$axios.get("/airs", { params: form }).then(res => {
+          // 机票数据
+          this.flightsData = res.data;
+          // 机票总数量
+          this.page.total = this.flightsData.flights.length;
+
+          // 需要实现 数组分页  this.flightsData.flights
+          // 记公式
+          // let list =totalList.slice()  使用它
+          // let list =totalList.slice(
+          // (当前的页码-1)*当前的页容量 ),
+          // 当前的页码* 当前的页容量
+          // )
+          // this.currentFlights = this.flightsData.flights.slice(
+          //   (this.page.currentPage - 1) * this.page.pageSize, // 0
+          //   this.page.currentPage * this.page.pageSize // 2
+          // );
+          this.currentFlights = this.flightsData.flights.slice(
+            (this.page.currentPage - 1) * this.page.pageSize,
+            this.page.currentPage * this.page.pageSize
+          );
+        });
+      } else {
+        this.currentFlights = this.flightsData.flights.slice(
+          (this.page.currentPage - 1) * this.page.pageSize,
+          this.page.currentPage * this.page.pageSize
+        );
+      }
+    },
+
+    // 分页数组改变触发
+    handleSizeChange(value) {
+      this.page.pageSize = value;
+      this.getList()
+    },
+
+    // 当前页码改变触发事件
+    handleCurrentChange(value) {
+      this.page.currentPage = value;
+
+      this.getList()
+    }
   },
   mounted() {
-      let form = this.$route.query
-      this.$axios.get('/airs',{ params: form })
-        .then(res => {
-          console.log(res)
-          this.flightsData = res.data
-          console.log(this.flightsData)
-        })
+    this.getList(true);
   }
 };
 </script>
